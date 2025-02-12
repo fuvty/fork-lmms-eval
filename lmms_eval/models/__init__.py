b8968662fcc020c7083bc7cf9cf8ef777de0fbb4
Tianyu Fu <fuvty@outlook.com>
2025-02-12 16:40:18 +0800

    [update] support vila

--- lmms_eval/models/__init__.py
+++ lmms_eval/models/__init__.py
@@ -46,6 +46,7 @@ AVAILABLE_MODELS = {
     "videoChatGPT": "VideoChatGPT",
     "video_llava": "VideoLLaVA",
     "vila": "VILA",
+    "vila_internal": "VILA",
     "xcomposer2_4KHD": "XComposer2_4KHD",
     "internvideo2": "InternVideo2",
     "xcomposer2d5": "XComposer2D5",
